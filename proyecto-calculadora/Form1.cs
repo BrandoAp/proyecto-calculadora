707156b8f07ecdfe221b825187c3bd50cc793aef
BrandoAp <avilabrando14@gmail.com>
2024-10-19 17:18:13 -0500

    Agregar archivos de proyecto.

---
+++ proyecto-calculadora/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proyecto_calculadora
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = @"Server=.\sqlexpress;Database=calculadora;Trusted_Connection=true;";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+                MessageBox.Show("Conexion Establecida");
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Error el intentar establecer la conexion ", ex.Message);
+            }
+
+        }
+    }
+}
